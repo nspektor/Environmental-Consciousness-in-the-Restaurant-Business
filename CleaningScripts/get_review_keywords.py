fc0e071a0e0769ebdb98fc793e281f1cacd5a695
nspektor <nellie.spektor@gmail.com>
2019-04-17 11:06:53 -0400

    generate csv in big_yelp_data with name, reviews, green rating

--- CleaningScripts/get_review_keywords.py
+++ CleaningScripts/get_review_keywords.py
@@ -1,35 +1,41 @@
 # to combine review.csv with all_clean_restaurants.csv
 # produces all text for each unique business_id
 
-import csv
 import pandas as pd
-import numpy as np
 
-rdf = pd.read_csv('DataSources/mini_review.csv', header=0)
+# rdf = pd.read_csv('DataSources/mini_review.csv', header=0)
+rdf = pd.read_csv('big_yelp_data/review.csv', header=0)
 bdf = pd.read_csv('DataSources/all_clean_restaurants.csv', header=0)
 greendf = pd.read_csv('DataSources/business_green.csv', header=0)
-mybdf = bdf.drop_duplicates(subset=['business_id'])
-mybdf['categories'] = mybdf['categories'].str.replace(',', ' ')
+bdf = bdf.drop_duplicates(subset=['business_id'])
+bdf['categories'] = bdf['categories'].str.replace(',', ' ')
+
+rdf = rdf[['business_id', 'text']]
+rdf['text'] = rdf['text'].str.replace(',', ' ')
+
+print("bdf:\n" , bdf.info())
+result_df = rdf.groupby('business_id')['text'].apply(list).to_frame().reset_index()
+print("result_df:\n" , result_df.get(0))
+businesses_and_reviews = bdf.merge(result_df, left_on='business_id', right_on='business_id', how='inner', suffixes=['_biz', '_test'])
+businesses_and_reviews = businesses_and_reviews.groupby('name')['text'].apply(list).to_frame().reset_index()
+
+print("businesses_and_reviews:\n" , businesses_and_reviews.head(10))
+bdf.to_csv('mini_yelp_data/bdf.csv')
+
+businesses_and_reviews.to_csv('mini_yelp_data/businesses_and_reviews.csv')
+result_df.to_csv('mini_yelp_data/result_df.csv')
+
+green = pd.read_csv('DataSources/clean_green.csv', header=0, delimiter=',')
+
+businesses_and_reviews = businesses_and_reviews.sort_values(['name'])
+green = green.sort_values(['Name'])
+
+testmerge = businesses_and_reviews.merge(green, left_on='name', right_on='Name', how='left', suffixes=['_b', '_g'])
+testmerge = testmerge[['name', 'text', 'rating']]
+print("green: ",testmerge['rating'].sum())
+print("not green: ",testmerge['rating'].isnull().sum())
+testmerge.to_csv('big_yelp_data/name_review_green.csv')
 
 
-myrdf = rdf[['business_id', 'text']]
-myrdf['text'] = myrdf['text'].str.replace(',', ' ')
-# run nltk preprocessing on text
 
 
-
-
-test = myrdf.groupby('business_id')['text'].apply(list).to_frame().reset_index()
-
-mergedf = mybdf.merge(test, left_on='business_id', right_on='business_id', how='inner', suffixes=['_biz', '_test'])
-#merge with green data
-
-grouped = mergedf.groupby('name')['text'].apply(list).to_frame().reset_index()
-
-print(grouped)
-grouped.to_csv('mini_yelp_data/grouped_by_name.csv')
-
-test['green'] = np.random.randint(0, 2, test.shape[0])
-
-mergedf.to_csv('mini_yelp_data/mini_merged_yelp.csv')
-test.to_csv('mini_yelp_data/mini_merged_reviews.csv')
